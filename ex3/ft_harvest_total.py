b870d2e6ea368f85c7341beec1f7a7a3b71ab7c6
Rhyan <rhyanclarck123@gmail.com>
2026-03-27 09:57:10 +0300

    ex3

---
+++ ex3/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total() -> None:
+    var1 = int(input("Day 1 harvest: "))
+    var2 = int(input("Day 2 harvest: "))
+    var3 = int(input("Day 3 harvest: "))
+    total = var1 + var2 + var3
+    print("Total harvest:", total)
